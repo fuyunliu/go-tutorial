d5f4a864feb6457dd101fc8b71781fabd59cb314
刘福云 <920507252@qq.com>
2019-05-05 11:22:52 +0800

    2019-05-05

---
+++ functions.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+var c, python, java bool
+var i, j = 1, 2
+
+func add(x int, y int) int {
+	return x + y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
+func main() {
+	fmt.Println(add(42, 13))
+	a, b := swap("world", "hello")
+	fmt.Println(a, b)
+	fmt.Println(split(17))
+	var i = 2
+	fmt.Println(i, c, python, java)
+}
